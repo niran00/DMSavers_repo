0ac216e6944fb795dde5c15bfe992372413b0b6c
Niran <niran.millet@gmail.com>
2021-02-10 20:35:33 +0700

    normal front

--- wp-content/themes/DMTheme/front-page.php
+++ wp-content/themes/DMTheme/front-page.php
@@ -7,7 +7,7 @@
 
 
 <div class="container">
-		<h1 class="pt-3 pb-3"><?php the_title();  ?>qwerrty123 </h1>
+		<h1 class="pt-3 pb-3"><?php the_title();  ?> </h1>
 <?php if(have_posts()) : while(have_posts()) : the_post();?> 
 
 

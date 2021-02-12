2a3b4acf8486b69096e245183451bae259a6904e
Niran <niran.millet@gmail.com>
2021-02-12 20:29:55 +0700

    commentout

--- wp-content/themes/DMTheme/front-page.php
+++ wp-content/themes/DMTheme/front-page.php
@@ -6,7 +6,9 @@
 
 
 <div class="container">
-		<!-- <h1 class="pt-3 pb-3"><?php the_title();  ?></h1> -->
+		<!-- <h1 class="pt-3 pb-3">
+		
+		<?php /* the_title(); */  ?></h1> -->
 <?php if(have_posts()) : while(have_posts()) : the_post();?> 
 
 
